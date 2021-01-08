0aa54f81262f56d7a5e616980a01e643877243fe
Jelmer Wiersma <jelmer.wiersma@student.rocva.nl>
2021-01-08 12:13:35 +0100

    opdrachten

---
+++ 3-Arrays/taak02 - Array waarde wijzigen/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+  $myDogNames = array("Lupa", "Bliss", "Tonka");
+  $myDogNames[2] = 'Ramsus';
+  echo $myDogNames[2];
+?>
+
+</body>
+</html> 
---
+++ 3-Arrays/taak03 - Array waarde toevoegen/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+  $myHeroes = array("Daredevil");
+  array_push($myHeroes, 'Batman');
+  $myHeroes[2] = 'The Flash';
+  print_r($myHeroes);
+?>
+
+</body>
+</html> 
