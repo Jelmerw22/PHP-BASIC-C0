fc325c91f7d70d0e65a262f99ac077cfb3d6cbc3
Jelmer Wiersma <jelmer.wiersma@student.rocva.nl>
2021-01-15 11:27:25 +0100

    verbetering

--- 5-Conditional_Statements/taak02 - if conditional/index.php
+++ 5-Conditional_Statements/taak02 - if conditional/index.php
@@ -21,10 +21,10 @@ if($test1 > $test2) {
 if($test1 < $test2) {
     echo "Test1 is KLEINER dan test2!" . "<br>";
 }
-if($test1 > "or" ==  $test2) {
+if($test1 >=  $test2) {
     echo "Test1 is GROTER of GELIJK aan test2!" . "<br>";
 }
-if($test1 < "or" == $test2) {
+if($test1 <= $test2) {
     echo "Test1 is KLEINER of GELIJK aan test2!" . "<br>";
 }
 ?>
