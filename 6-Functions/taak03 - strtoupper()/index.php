6891e6c6e747a4b6b1fcb678e86e0443e93ee271
Jelmer Wiersma <jelmer.wiersma@student.rocva.nl>
2021-01-22 11:03:22 +0100

    taak: taak03 - strtoupper()

---
+++ 6-Functions/taak03 - strtoupper()/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+function telAlleWoorden(){
+    $woorden ="Mijn blauwe auto is een mooie snelle wagen";
+    $lengtezin =strlen($woorden);
+    echo $lengtezin;
+}
+
+telAlleWoorden();
+
+?>
+
+</body>
+</html> 
