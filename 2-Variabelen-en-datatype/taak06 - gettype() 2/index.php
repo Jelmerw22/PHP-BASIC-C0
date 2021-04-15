48b2f36f837a744886cb7fead7cd60d4bbbb7ed2
Jelmer Wiersma <jelmer.wiersma@rocva.nl>
2021-04-15 11:03:38 +0200

    hotfixes

--- 2-Variabelen-en-datatype/taak06 - gettype() 2/index.php
+++ 2-Variabelen-en-datatype/taak06 - gettype() 2/index.php
@@ -8,11 +8,11 @@ $getal = '7';
 $decimaalgetal = '7.7';
 $test1 = true;
 $test2 = null;
-echo gettype($voormaan)
-echo gettype($getal)
-echo gettype($decimaalgetal)
-echo gettype($test1)
-echo gettype($test2)
+echo gettype($voormaan);
+echo gettype($getal);
+echo gettype($decimaalgetal);
+echo gettype($test1);
+echo gettype($test2);
 ?>
 
 </body>
--- 2-Variabelen-en-datatype/taak08 - data en html combineren/index.php
+++ 2-Variabelen-en-datatype/taak08 - data en html combineren/index.php
@@ -4,8 +4,8 @@
 
 <?php
 $voornaam1 = "Jelmer";
-$voornaam2 = "Diego"
-$voornaam3 = "Hayder"
+$voornaam2 = "Diego";
+$voornaam3 = "Hayder";
 echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>";
 ?>
 
