918487ea7f13813c3ad62344080836a42126e46e
Jelmer Wiersma <jelmer.wiersma@rocva.nl>
2021-04-15 10:55:25 +0200

    meer opdrachten

---
+++ 2-Variabelen-en-datatype/taak06 - gettype() 2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$voormaan = 'Tom';
+$getal = '7';
+$decimaalgetal = '7.7';
+$test1 = true;
+$test2 = null;
+echo gettype($voormaan)
+echo gettype($getal)
+echo gettype($decimaalgetal)
+echo gettype($test1)
+echo gettype($test2)
+?>
+
+</body>
+</html> 
