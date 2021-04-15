d0bcda6ea6940bf1f76ccfaa1201ce900161bbbf
Jelmer Wiersma <jelmer.wiersma@rocva.nl>
2021-04-15 10:56:07 +0200

    opdracht

---
+++ 2-Variabelen-en-datatype/taak08 - data en html combineren/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$voornaam1 = "Jelmer";
+$voornaam2 = "Diego"
+$voornaam3 = "Hayder"
+echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>";
+?>
+
+</body>
+</html> 
